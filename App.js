71f937671e4af2d3435a3489d4b9264b2077eeed
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2023-01-18 10:00:18 -0300

    instalando o pacote, api de mapas e localização inicial
    ajustando o app.js para a visualização do mapview

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicial = {
+    latitude: -23.52618,
+    longitude: -46.54027,
+    latitudeDelta: 0.0222,
+    longitudeDelta: 0.00121,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={regiaoInicial} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
